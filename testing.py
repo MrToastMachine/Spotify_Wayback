9caba3b4634fa20d83c7da4bd106da7529f17c01
MrToastMachine <alexfomahony@gmail.com>
2023-10-27 18:05:45 +0100

    changed content of test file

--- testing.py
+++ testing.py
@@ -1,13 +1,1 @@
-import pandas as pd
-
-# Replace 'your_file.xlsx' with the actual path to your Excel file
-file_path = 'single_duplicates_only.xlsx'
-
-# Read the Excel file
-df = pd.read_excel(file_path)
-
-# Extract the "uri" column as a Python array
-uri_array = df['spotify_track_uri'].tolist()
-
-# Print or use the uri_array as needed
-print(uri_array)
+print("This is the file for testing")
